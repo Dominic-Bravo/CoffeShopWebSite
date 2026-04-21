ec17806248c3b3ed348de814152b6a6c3860df20
Dominic-Bravo <dominicianbravo017@gmail.com>
2026-04-21 10:49:14 +0800

    modifies navbar, adddes navbar types and texts margins

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,35 +1,37 @@
 import { hoverScale } from "../styles/customStyle";
 import logo from "../assets/images/coffeeLogo2.png"
+import type { NavbarItem } from "../types/navbartypes";
+
+const navbarItems: NavbarItem[] = [
+  { label: "Stories", href: "#" },
+  { label: "Coffee", href: "#" },
+  { label: "Equipment", href: "#" },
+  { label: "Store", href: "#" },
+];
+
 
 function Navbar() {
   return (
     <nav className="p-6">
       <div className="container mx-auto flex items-center justify-between">     
-        <div className="flex items-center ml-10 gap-3">
+        <div className="flex items-center gap-3">
             <img 
                 src={logo} 
                 alt="Coffee Logo" 
-                className="w-12 h-12 object-contain"
+                className="w-20 h-20 object-contain"
             />
             <div className="text-2xl font-bold tracking-wide text-amber-800">
                 Coffee Shop
             </div>
         </div>
-        <ul className="flex space-x-10 text-xl ml-15 font-medium">
-            <li className={`${hoverScale}`}>
-                <a href="#" className="text-amber-900 hover:text-amber-700">Stories</a>
-            </li>
-            <li className={`${hoverScale}`}>
-                <a href="#" className="text-amber-900 hover:text-amber-700">Coffee</a>     
-            </li>
-            <li className={`${hoverScale}`}>
-                <a href="#" className="text-amber-900 hover:text-amber-700">Equipment</a>
-            </li>
-            <li className={`${hoverScale}`}>
-                <a href="#" className="text-amber-900 hover:text-amber-700">Store</a>z
-            </li>
-        </ul>   
-        <button className={`font-bold bg-amber-100 text-green-900 mr-10 px-4 py-4 w-24 rounded-full hover:bg-amber-110 ${hoverScale}`}>Order</button>
+            <ul className="flex space-x-10 mr-15 text-xl font-medium">
+                {navbarItems.map((item) => (
+                    <li key={item.label} className={`${hoverScale}`}>
+                        <a href={item.href} className="text-amber-900 hover:text-amber-700">{item.label}</a>
+                    </li>
+                ))}
+            </ul>   
+        <button className={`font-bold bg-amber-100 text-green-900 px-4 py-4 w-24 rounded-full hover:bg-amber-110 ${hoverScale}`}>Order</button>
         </div>
 
     </nav>
--- src/components/test.tsx
+++ src/components/test.tsx
@@ -10,7 +10,7 @@ function Hello() {
         <h1 className={`text-6xl font-bold text-amber-900 italic ${hoverScale}`}>
           “Brewed to perfection. Served with passion.”
         </h1>
-        <p className="text-amber-900 mt-4 indent-10 text-2xl mt-15 transition-transform duration-500 ease-out hover:scale-105">
+        <p className="text-amber-900 mt-4 indent-15 text-2xl mt-15 transition-transform duration-500 ease-out hover:scale-105">
           At our coffee shop, every cup tells a story. From carefully sourced beans to expertly crafted drinks, we turn simple moments into something special. Whether you're here to work, relax, or catch up with friends—there’s always a perfect brew waiting for you.
         </p>
         <div className="flex space-x-4 mt-10">
